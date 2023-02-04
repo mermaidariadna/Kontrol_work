ab77596c6e6ba885c2d511fa1013b98052b14699
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 20:39:49 +0300

    new task0

--- task01.cpp
+++ task01.cpp
@@ -1,11 +1,18 @@
 #include "task01.hpp"
 
-void fillRandom(int* firstElementArr, int size) {
+void fillRandom(int *ar, int size, int min, int max){
         srand(time(nullptr));
-        for (int i = 0; i < size; i++) {
-                *(firstElementArr + i) = random() % 200;
+        for (int i = 0; i < size; i++){
+                int num = rand()%1000;
+                if (num <= max && num >= min){
+                        ar[i] = num;
+                }
+                else{
+                        i = i - 1;
+                }
         }
 }
+
 
 int test_task01(int* arr, int size){
        fillRandom(&arr[0], size);
