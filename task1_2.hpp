bbfc815584b188ab3ae3511d519fbb65703f57a7
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 21:09:27 +0300

    Create task1_2.hpp

---
+++ task1_2.hpp
@@ -0,0 +1,7 @@
+#ifndef _task1_2_hpp_
+#define _task1_2_hpp_
+#include <iostream>
+using namespace std;
+int selectSort(int *arr, int n, int m);
+int test_task1_2(int* arr, int n, int m);
+#endif /*_task1_2_hpp_*/
