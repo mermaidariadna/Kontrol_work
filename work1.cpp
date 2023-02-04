e7614cf9a65031e513e84e0ce086cf21999245c7
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 20:38:01 +0300

    task1

--- work1.cpp
+++ work1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <time.h>
 #include "task01.hpp"
+#include "task1_1.hpp"
 int main(){
         int arr[100], n = 10;
-        fillRandom(&arr[0], n);
+        fillRandom(ar, 1000, 0, 200);
         for(int i = 0; i < n; i++){
                 std::cout << i + 1 << " " << arr[i] << std::endl;
         }       //test_task01(arr, n);  
+        showEl(arr, n, N, M);
 }
