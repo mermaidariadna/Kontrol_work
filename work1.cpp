b92ea3a332a05cc7ab4cbb8db1e033eb294d95ad
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 21:07:43 +0300

    task1.2

--- work1.cpp
+++ work1.cpp
@@ -2,11 +2,15 @@
 #include <time.h>
 #include "task01.hpp"
 #include "task1_1.hpp"
+#include "task1_2.hpp"
 int main(){
-        int arr[100], n = 10;
-        fillRandom(ar, 1000, 0, 200);
+        int arr[100], n = 10, N = 10, M = 10;
+        fillRandom(arr, 100, 0, 200);
         for(int i = 0; i < n; i++){
                 std::cout << i + 1 << " " << arr[i] << std::endl;
-        }       //test_task01(arr, n);  
+        }       //test_task01(arr, n);
         showEl(arr, n, N, M);
+        selectSortShow(arr, N, M);
+        test_task1_2(arr,N , M);
 }
+
