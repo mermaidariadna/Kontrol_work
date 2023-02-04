592f0339932c9b3b50df221ab2daf0c3638953b9
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 21:23:47 +0300

    plus task2

--- work1.cpp
+++ work1.cpp
@@ -3,6 +3,7 @@
 #include "task01.hpp"
 #include "task1_1.hpp"
 #include "task1_2.hpp"
+#include "task2.hpp"
 int main(){
         int arr[100], n = 10, N = 10, M = 10;
         fillRandom(arr, 100, 0, 200);
@@ -12,5 +13,9 @@ int main(){
         showEl(arr, n, N, M);
         selectSortShow(arr, N, M);
         test_task1_2(arr,N , M);
+        int ar[] = {0, 1, 82, 3, 44, 5, 6, 27, 8, 9};
+        getMax(ar, 10);
+        getMin(ar, 10);
 }
+
 
