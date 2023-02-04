321699486fc1f39e5495fd443f9dea46d7e1d5d6
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 20:41:11 +0300

    Update task01.hpp

--- task01.hpp
+++ task01.hpp
@@ -2,6 +2,6 @@
 #define _task01_hpp_
 #include <iostream>
 #include <time.h>
-void fillRandom(int* arr, int size);
+void fillRandom(int *ar, int size, int min, int max);
 int test_task01(int* arr, int size);
 # endif /*_task01_hpp_*/
