59f10ee7cbae5adbf3df6fa58121578eebd62dcd
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 20:42:14 +0300

    Create task1_1.hpp

---
+++ task1_1.hpp
@@ -0,0 +1,7 @@
+#ifndef _task1_1_hpp_
+#define _task1_1_hpp_
+#include <iostream>
+void fillRandom(int *ar, int size, int min, int max);
+int showEl(const int* ar, int size, int N = 10, int M = 10);
+int test_task1_1(const int*, int, int, int);
+#endif /*_task1_1_hpp_*/
