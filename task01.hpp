d23047bdc3b4677ec3cc6446a680af03a12357b9
mermaidariadna <121664445+mermaidariadna@users.noreply.github.com>
2023-02-04 20:16:16 +0300

    Create task01.hpp

---
+++ task01.hpp
@@ -0,0 +1,7 @@
+#ifndef _task01_hpp_
+#define _task01_hpp_
+#include <iostream>
+#include <time.h>
+void fillRandom(int* arr, int size);
+int test_task01(int* arr, int size);
+# endif /*_task01_hpp_*/
